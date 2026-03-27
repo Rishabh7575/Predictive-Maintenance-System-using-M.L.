caa538d4e03faceda4754c2f2434d1e7adb55335
Rishabh Kumar <kumarvrishabh700@gmail.com>
2026-03-28 00:19:16 +0530

    No more message

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,12 +1,12 @@
-# from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report
 
-# def evaluate_model(model, X_test, y_test):
-#     preds = model.predict(X_test)
-#     return {
-#         "confusion_matrix": confusion_matrix(y_test, preds),
-#         "report": classification_report(y_test, preds)
-#     }
+def evaluate_model(model, X_test, y_test):
+    preds = model.predict(X_test)
+    return {
+        "confusion_matrix": confusion_matrix(y_test, preds),
+        "report": classification_report(y_test, preds)
+    }
 
 
 
-# #to be pushed to the repo, this file will contain functions to evaluate the performance of the trained model. It will include metrics such as confusion matrix and classification report.
+#to be pushed to the repo, this file will contain functions to evaluate the performance of the trained model. It will include metrics such as confusion matrix and classification report.
