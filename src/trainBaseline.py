780ab17da72e49d2840dc8687ca5a88b63649ac4
Rishabh Kumar <kumarvrishabh700@gmail.com>
2026-03-29 23:08:36 +0530

    trainBsLn

--- src/trainBaseline.py
+++ src/trainBaseline.py
@@ -1,17 +1,17 @@
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
 
-# def train_models(X, y):
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         X, y, stratify=y, test_size=0.2, random_state=42
-#     )
+def train_models(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, stratify=y, test_size=0.2, random_state=42
+    )
 
-#     scaler = StandardScaler()
-#     X_train = scaler.fit_transform(X_train)
-#     X_test = scaler.transform(X_test)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
 #     lr = LogisticRegression(max_iter=1000)
 #     lr.fit(X_train, y_train)
