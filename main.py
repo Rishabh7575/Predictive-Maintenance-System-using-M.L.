7af0d9606323808a1807063e838c4fb4b76aa729
Rishabh Kumar <kumarvrishabh700@gmail.com>
2026-03-30 11:57:43 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,48 +1,14 @@
-import sys
-import os
-sys.path.append(os.path.abspath("src"))
-
-from preprocessing import load_cmaps_data, add_rul
-from featureEngineering import add_rolling_features
+from src.preprocessing import load_cmaps_data, add_rul
+from src.featureEngineering import add_rolling_features
 
 path = "data/raw/train_FD001.txt"
-
-sensor_cols = [f"s{i}" for i in range(1, 22)]
+sensor_cols = [f"s_{i}" for i in range(1, 22)]
 
 df = load_cmaps_data(path)
+df = add_rul(df)
+df = add_rolling_features(df, sensor_cols)
+
+df = df.dropna()   # IMPORTANT
+
 print(df.head())
 print(df.shape)
-
-df = add_rul(df)
-print(df.head())
-
-df = add_rolling_features(df, sensor_cols)
-print(df.head())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# df = load_cmaps_data(path)
-# print(df.head())
-# print(df.shape())
-
-# df = add_rul(df)
-# print(df.head())
-
-# df = add_rolling_features(df, sensor_cols)
-# print(df.head())
