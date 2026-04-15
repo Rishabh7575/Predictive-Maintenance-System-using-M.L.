49e1c46dff609f2493205ff562c77baf6f5a9d65
Rishabh Kumar <kumarvrishabh700@gmail.com>
2026-04-15 23:19:04 +0530

    too mch commits

--- app/streamlitApp.py
+++ app/streamlitApp.py
@@ -2,21 +2,30 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-st.title("Predictive Maintenance System")
+st.title("🔧 Predictive Maintenance System")
 
+# Load model
 model = joblib.load("models/xgb_model.pkl")
 
-uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
+uploaded_file = st.file_uploader("Upload Sensor CSV", type=["csv"])
 
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
 
-    st.write("Input Data", df.head())
+    st.subheader("📊 Input Data")
+    st.dataframe(df.head())
 
-    preds = model.predict(df)
-    probs = model.predict_proba(df)[:, 1]
+#     try:
+#         preds = model.predict(df)
+#         probs = model.predict_proba(df)[:, 1]
 
-    df["Prediction"] = preds
-    df["Failure_Probability"] = probs
+#         df["Prediction"] = preds
+#         df["Failure_Probability"] = probs
 
-    st.write("Predictions", df.head())
+#         st.subheader("📈 Predictions")
+#         st.dataframe(df.head())
+
+#         st.success("Prediction completed successfully!")
+
+#     except Exception as e:
+#         st.error(f"Error: {e}")
